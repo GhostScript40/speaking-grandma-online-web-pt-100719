21f3347ea156d5067e7e56bbeadca46e321c5b65
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-10 09:21:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
    phrase == "I LOVE YOU GRANDMA!"
    return  "I LOVE YOU TOO PUMPKIN!"
   else
-    phrase.upcase
+    phrase == phrase.upcase
     return "NO NOT SINCE 1938"
     end 
 end
