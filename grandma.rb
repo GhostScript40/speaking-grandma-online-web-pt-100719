3ea172d8557e51e319d3da06ff6d14fa5fe301c9
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-10 09:21:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-   if phrase.capitalize
+   if !phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
    elsif
    phrase == "I LOVE YOU GRANDMA!"
